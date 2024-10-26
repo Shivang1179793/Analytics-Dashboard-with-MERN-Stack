672a287bf292a5b6f63a23974a1deac247e388bb
Shivang1179793 <shivangbaranwal4@gmail.com>
2024-10-26 19:31:48 +0530

    dashboard

--- client/src/components/BreakdownCharts.jsx
+++ client/src/components/BreakdownCharts.jsx
@@ -34,29 +34,52 @@ const BreakdownCharts = ({ transactions, yearlySalesTotal }) => {
         theme={{
           axis: {
             domain: { line: { stroke: theme.palette.secondary[200] } },
-            ticks: { line: { stroke: theme.palette.secondary[200], strokeWidth: 1 }, text: { fill: theme.palette.secondary[200] } },
+            legend: {
+              text: {
+                fill: theme.palette.secondary[200],
+              },
+            },
+            ticks: { line: { stroke: theme.palette.secondary[200], strokeWidth: 1 }, text: { fill: theme.palette.secondary[200] }, },
           },
           legends: { text: { fill: theme.palette.secondary[200] } },
           tooltip: { container: { color: theme.palette.primary.main } },
         }}
         colors={{ datum: "data.color" }}
         margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        sortByValue={true}
         innerRadius={0.45}
+        activeOuterRadiusOffset={8}
         borderWidth={1}
         borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
         enableArcLinkLabels={true}
         arcLinkLabelsTextColor={theme.palette.secondary[200]}
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: "color" }}
         arcLabelsSkipAngle={10}
         arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
         legends={[
           {
             anchor: "bottom",
             direction: "row",
+            justify: false,
+            translateX:0,
             translateY: 56,
+            itemsSpacing: 0,
             itemWidth: 85,
             itemHeight: 18,
+            itemTextColor: "#999",
+            itemDirection: "left-to-right",
+            itemOpacity: 1,
             symbolSize: 18,
             symbolShape: "circle",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: theme.palette.primary[500],
+                },
+              },
+            ],
           },
         ]}
       />
@@ -66,7 +89,8 @@ const BreakdownCharts = ({ transactions, yearlySalesTotal }) => {
         left="50%"
         color={theme.palette.secondary[400]}
         textAlign="center"
-        sx={{ transform: "translate(-50%, -50%)" }}
+        pointerEvents="none"
+        sx={{ transform: "translate(-50%, -100%)" }}
       >
         <Typography variant="h6">Total: ${yearlySalesTotal}</Typography>
       </Box>
