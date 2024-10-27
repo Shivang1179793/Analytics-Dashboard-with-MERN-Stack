f6c25df63e786ace1cb238003b6a38e5a39fcbba
Shivang1179793 <shivangbaranwal4@gmail.com>
2024-10-27 13:26:23 +0530

    dashboard

--- client/src/components/BreakdownCharts.jsx
+++ client/src/components/BreakdownCharts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ResponsivePie } from "@nivo/pie";
 import { Box, Typography, useTheme } from "@mui/material";
 
-const BreakdownCharts = ({ transactions, yearlySalesTotal }) => {
+const BreakdownCharts = ({ transactions, yearlySalesTotal,isDashboard=false }) => {
   const theme = useTheme();
 
   const colors = [
@@ -11,7 +11,6 @@ const BreakdownCharts = ({ transactions, yearlySalesTotal }) => {
     theme.palette.secondary[100],
     theme.palette.secondary[700],
   ];
-
   const formattedData = transactions.reduce((acc, { name, cost }) => {  
     const category = acc.find((item) => item.id === name);
     if (category) {
@@ -27,7 +26,13 @@ const BreakdownCharts = ({ transactions, yearlySalesTotal }) => {
     return acc;
   }, []);
   return (
-    <Box height="100%" position="relative">
+    <Box 
+      height={isDashboard ? "400px" : "100%"}
+      width={undefined}
+      minHeight={isDashboard ? "325px" : undefined}
+      minWidth={isDashboard ? "325px" : undefined}
+      position="relative"
+    >
       <ResponsivePie
         data={formattedData}
         theme={{
@@ -44,13 +49,17 @@ const BreakdownCharts = ({ transactions, yearlySalesTotal }) => {
           tooltip: { container: { color: theme.palette.primary.main } },
         }}
         colors={{ datum: "data.color" }}
-        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        margin={
+          isDashboard
+            ? { top: 40, right: 80, bottom: 100, left: 50 }
+            : { top: 40, right: 80, bottom: 80, left: 80 }
+        }
         sortByValue={true}
         innerRadius={0.45}
         activeOuterRadiusOffset={8}
         borderWidth={1}
         borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
-        enableArcLinkLabels={true}
+        enableArcLinkLabels={!isDashboard}
         arcLinkLabelsTextColor={theme.palette.secondary[200]}
         arcLinkLabelsThickness={2}
         arcLinkLabelsColor={{ from: "color" }}
@@ -61,8 +70,8 @@ const BreakdownCharts = ({ transactions, yearlySalesTotal }) => {
             anchor: "bottom",
             direction: "row",
             justify: false,
-            translateX:0,
-            translateY: 56,
+            translateX: isDashboard ? 20 : 0,
+            translateY: isDashboard ? 50 : 56,
             itemsSpacing: 0,
             itemWidth: 85,
             itemHeight: 18,
@@ -89,9 +98,14 @@ const BreakdownCharts = ({ transactions, yearlySalesTotal }) => {
         color={theme.palette.secondary[400]}
         textAlign="center"
         pointerEvents="none"
-        sx={{ transform: "translate(-50%, -100%)" }}
+        sx={{
+          transform: isDashboard
+            ? "translate(-75%, -170%)"
+            : "translate(-50%, -100%)",
+        }}
       >
-        <Typography variant="h6">Total: ${yearlySalesTotal}</Typography>
+        <Typography variant="h6">
+          {!isDashboard && "Total:"} ${yearlySalesTotal}</Typography>
       </Box>
     </Box>
   );
--- client/src/components/OverviewCharts.jsx
+++ client/src/components/OverviewCharts.jsx
@@ -2,7 +2,7 @@ import React, { useMemo } from "react";
 import { ResponsiveLine } from '@nivo/line';
 import { useTheme } from '@emotion/react';
 
-const OverviewCharts = ({ data, view }) => {
+const OverviewCharts = ({ data, view,isDashboard=false }) => {
   const theme = useTheme();
 
   const [totalSalesLine, totalUnitsLine] = useMemo(() => {
@@ -35,7 +35,6 @@ const OverviewCharts = ({ data, view }) => {
     Object.keys(totalUnitsData).forEach(month => {
       totalUnitsLine.data.push({ x: month, y: totalUnitsData[month] });
     });
-
     return [totalSalesLine, totalUnitsLine];
   }, [data, theme]);
 
@@ -56,22 +55,31 @@ const OverviewCharts = ({ data, view }) => {
       yScale={{ type: "linear", min: "auto", max: "auto", stacked: false, reverse: false }}
       yFormat=" >-.2f"
       curve="catmullRom"
-      enableArea={false}
+      enableArea={isDashboard}
+      axisTop={null}
+      axisRight={null}
       axisBottom={{
+        format: (v) => {
+          if (isDashboard) return v.slice(0, 3);
+          return v;
+        },
         orient: "bottom",
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "Month",
+        legend: isDashboard ? "" : "Month",
         legendOffset: 36,
         legendPosition: "middle",
       }}
       axisLeft={{
         orient: "left",
+        tickValues: 5,
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: view === "sales" ? "Total Sales" : "Total Units",
+        legend: isDashboard
+          ? ""
+          : `Total ${view === "sales" ? "Revenue" : "Units"} for Year`,
         legendOffset: -60,
         legendPosition: "middle",
       }}
@@ -81,31 +89,38 @@ const OverviewCharts = ({ data, view }) => {
       pointColor={{ theme: "background" }}
       pointBorderWidth={2}
       pointBorderColor={{ from: "serieColor" }}
+      pointLabelYOffset={-12}
       useMesh={true}
-      legends={[
-        {
-          anchor: "bottom-right",
-          direction: "column",
-          translateX: 30,
-          translateY: -40,
-          itemsSpacing: 0,
-          itemDirection: "left-to-right",
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: "circle",
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemBackground: "rgba(0, 0, 0, .03)",
-                itemOpacity: 1,
+      legends={
+        !isDashboard
+          ? [
+              {
+                anchor: "bottom-right",
+                direction: "column",
+                justify: false,
+                translateX: 30,
+                translateY: -40,
+                itemsSpacing: 0,
+                itemDirection: "left-to-right",
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: "circle",
+                symbolBorderColor: "rgba(0, 0, 0, .5)",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemBackground: "rgba(0, 0, 0, .03)",
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
               },
-            },
-          ],
-        },
-      ]}
+            ]
+          : undefined
+      }
     />
   ) : (
     "Loading..."
